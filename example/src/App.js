678731af29b35094f4cdbb6c99fb45391a05743c
ericpuskas <eric.puskas@lynxsolutions.eu>
2019-11-19 09:05:01 +0200

    Init commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
+import { HelloWorld, ByeWorld } from "@ericpuskas/react-to-npm";
 
-import ExampleComponent from '@ericpuskas/react-to-npm'
+const App = () => {
+  const [title, setTitle] = useState("");
+  const handleChange = () => {
+    setTitle("Hello World");
+  };
+  return (
+    <div>
+      {title}
+      <button onClick={handleChange}> Click me </button>
+      <HelloWorld />
+      <ByeWorld />
+    </div>
+  );
+};
 
-export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
-  }
-}
+export default App;
---
+++ src/components/ByeWorld.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ByeWorld = () => {
+  return (
+    <div>
+      <h1> Bye World</h1>
+    </div>
+  );
+};
+
+export default ByeWorld;
